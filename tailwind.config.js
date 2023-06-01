be7cbe86bd410ad0627cb9738469ee6366ac7f4b
Qusu4l <biwanski94@gmail.com>
2023-06-01 20:47:01 +0200

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['*.{html,js}'],
+	theme: {
+		extend: {},
+		fontFamily: {
+			leagueSpartan: ['League Spartan'],
+		},
+	},
+	plugins: [],
+}
